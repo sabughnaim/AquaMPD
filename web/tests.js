56d3b42a289fd46d20a3487862740278c9470dc8
Michael Gofron <gofronm@yahoo.com>
2014-12-01 17:01:31 -0600

    Added Some basic frameworks for testing
    Where were you Melanie!!! Where were you!

--- web/tests.js
+++ web/tests.js
@@ -5,11 +5,26 @@ function isEven(val){
     return val %2 == 0;
 }
 
+function checkUserCreated(num){
+    var iswrong;
+    var $wrongPhoneNum = $("#phone-number-incorrect");
+    $wrongPhoneNum.on("popup",function (){
+        iswrong = 0; 
+    })
+    create_new_user(num);
+    return iswrong; 
+    
+}
+
 test('isEven()',function(){
     ok(isEven(0), 'Zero is an even number');
     ok(isEven(2), 'So is two');
     ok(isEven(-4), 'So is negative four');
     ok(!isEven(1), 'One is not an even number');
     ok(!isEven(-7), 'Neither is negative seven');
+    ok(!isEven(6), '6 should be even');
 })
-        
+
+test('create_new_user', function(){
+    ok(!checkUserCreated(0), 'The user was not created');
+})     
